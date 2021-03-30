097fccd3344391ef02ec91565503163fa895dea3
bluskai <37144222+bluskai@users.noreply.github.com>
2021-03-30 21:21:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git linkease https://github.com/linkease/ddnsto-openwrt' feeds.conf.default
