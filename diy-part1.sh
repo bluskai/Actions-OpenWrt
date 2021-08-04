addd7f3a74def3f540febdd50c59e8e3540fe4b2
bluskai <37144222+bluskai@users.noreply.github.com>
2021-08-04 22:32:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,15 +11,9 @@
 #
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Add nas-packages feed source
-echo >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-
-# Add helloworld feed source
-echo >> feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >> feeds.conf.default
