323641ae731a5a75f629ec10314d026c0512eddf
bluskai <37144222+bluskai@users.noreply.github.com>
2024-01-10 15:44:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
 
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
